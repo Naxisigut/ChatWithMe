eb0bfdc9dd6e2a1201d472c4b6b6117c9631a1ef
“lcc” <chengcheng.li@51hgp.com>
2023-07-27 12:53:21 +0800

    feat: openai

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-// import { Configuration, OpenAIApi } from 'openai';
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-
-// const chatCompletion = await openai.createChatCompletion({
-//   model: "gpt-3.5-turbo",
-//   messages: [{role: "user", content: "Hello world"}],
-// });
-// console.log(chatCompletion.data.choices[0].message);
 import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
 dotenv.config()
 
-console.log(process.env);
-console.log(typeof process.env.test);
+const configuration = new Configuration({
+  basePath: 'https://api.chatanywhere.com.cn',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const Messages: {role: 'user' | 'assistant', content: string}[] = []
+;(async ()=>{
+  while(true){
+
+    const chatCompletion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: "Hello world"}],
+    });
+    const message = chatCompletion.data.choices[0].message 
+    // Messages.push(message)
+    console.log(message);
+  }
+})()
+
+
+// console.log(process.env.OPENAI_API_KEY);
